7706e1a4e0fd7d1dd49b29c9cca43fa6ed4c047a
Ivo Rahnev <Hero.ZeroDoku@gmail.com>
2025-03-12 22:12:27 +0200

    Add project files.

---
+++ C.cpp
@@ -0,0 +1,14 @@
+﻿// C.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory>
+
+int main() {
+   
+
+
+    return 0;
+}
